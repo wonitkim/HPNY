3f91824c70d9423e7074eedfa8f28b659afdbe54
wonit <jwk0341@gmail.com>
2023-01-14 21:52:15 +0900

    [mod] 라우터 수정
    - upload,post 관련 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Board from './Board.js';
+import Posts from './Posts.js';
+import Upload from './Upload.js';
 
 export default function App({ $target }) {
   this.route = () => {
@@ -7,6 +9,11 @@ export default function App({ $target }) {
 
     if (pathname === '/') {
       new Board({ $target }).render();
+    } else if (pathname === '/upload') {
+      new Upload({ $target }).render();
+    } else if (pathname.indexOf('/post/') !== -1) {
+      const [, , p_id] = pathname.split('/');
+      new Posts({ $target, p_id }).render();
     }
   };
 
