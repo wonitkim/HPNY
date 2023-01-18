692ad0ad99a2427d6a65c5b725e76020b981ad2c
jwkim <92710923+jwkim0341@users.noreply.github.com>
2023-01-18 09:13:23 +0900

    [mod] 프리티어 설정으로 인한 코드 변경

--- src/PostDetailPage.js
+++ src/PostDetailPage.js
@@ -1,13 +1,11 @@
-import { addComment, deletePost, getPostDetail } from './apis/api.js';
-import CommentList from './CommentList.js';
-import Header from './Header.js';
-import { routeChange } from './router.js';
+import { addComment, deletePost, getPostDetail } from "./apis/api.js";
+import CommentList from "./CommentList.js";
+import Header from "./Header.js";
+import { routeChange } from "./router.js";
 
 export default function PostDetailPage({ $target, p_id }) {
-  this.state = { p_id };
-
-  const $page = document.createElement('section');
-  $page.id = 'post_detail_page';
+  const $page = document.createElement("section");
+  $page.id = "post_detail_page";
 
   this.render = () => {
     $target.appendChild($page);
@@ -19,36 +17,44 @@ export default function PostDetailPage({ $target, p_id }) {
 
   const dateToStr = (date) => {
     let year = date.getFullYear();
-    let month = date.getMonth() > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
-    let day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
-    return year + '.' + month + '.' + day;
+    let month =
+      date.getMonth() > 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1);
+    let day = date.getDate() > 9 ? date.getDate() : "0" + date.getDate();
+    return year + "." + month + "." + day;
   };
 
-  $page.addEventListener('click', async (e) => {
-    if (e.target.id === 'leave_comment_btn' && e.target.classList.contains('active')) {
+  $page.addEventListener("click", async (e) => {
+    if (
+      e.target.id === "leave_comment_btn" &&
+      e.target.classList.contains("active")
+    ) {
       let comment = e.target.previousElementSibling.value;
       await leaveComment(comment);
     }
 
-    if (e.target.id === 'post_delete') {
-      let option = { method: 'DELETE' };
+    if (e.target.id === "post_delete") {
+      let option = { method: "DELETE" };
       let result = await deletePost(p_id, option);
-      if (result.code === 200) routeChange('/', null, 're');
+      if (result.code === 200) routeChange("/", null, "re");
+    }
+
+    if (e.target.id === "post_edit") {
+      routeChange(`/edit/${p_id}`);
     }
   });
 
-  $page.addEventListener('keyup', async (e) => {
-    let btn = document.querySelector('#leave_comment_btn');
-    let input = document.querySelector('#comment_input');
+  $page.addEventListener("keyup", async (e) => {
+    let btn = document.querySelector("#leave_comment_btn");
+    let input = document.querySelector("#comment_input");
 
-    if (e.target.id === 'comment_input' && e.key !== 'Enter') {
-      if (e.target.value === '') btn.classList.remove('active');
-      else btn.classList.add('active');
-    } else if (e.key === 'Enter') {
-      if (btn.classList.contains('active')) {
+    if (e.target.id === "comment_input" && e.key !== "Enter") {
+      if (e.target.value === "") btn.classList.remove("active");
+      else btn.classList.add("active");
+    } else if (e.key === "Enter") {
+      if (btn.classList.contains("active")) {
         await leaveComment(input.value);
       } else {
-        return alert('댓글을 등록해주세요.');
+        return alert("댓글을 등록해주세요.");
       }
     }
   });
@@ -60,8 +66,8 @@ export default function PostDetailPage({ $target, p_id }) {
 
   const leaveComment = async (comment) => {
     let option = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content: comment }),
     };
     let result = await addComment(p_id, option);
@@ -72,16 +78,16 @@ export default function PostDetailPage({ $target, p_id }) {
       // //댓글 입력란 초기화 및 추가된 댓글 보이도록
       // document.querySelector('#comment_input').value = '';
       location.reload();
-      document.querySelector('.post_detail_inner').scrollTop =
-        document.querySelector('.post_detail_inner').clientHeight;
+      document.querySelector(".post_detail_inner").scrollTop =
+        document.querySelector(".post_detail_inner").clientHeight;
     } else {
-      return alert('댓글을 등록하지 못했습니다. \n다시 시도해주세요.');
+      return alert("댓글을 등록하지 못했습니다. \n다시 시도해주세요.");
     }
   };
 
   const selectPostDetail = async () => {
     await getPost();
-    new Header({ $target: $page, type: 'detail' });
+    new Header({ $target: $page, type: "detail" });
     let date = new Date(this.state.post.createdAt);
     $page.innerHTML += `<div class="post_detail_inner">
       <artice class="post_article">
@@ -108,7 +114,11 @@ export default function PostDetailPage({ $target, p_id }) {
       </div>
     </div>
     `;
-    new CommentList({ $target: $page.querySelector('.post_comment_wrap'), p_id: p_id, init: this.state.comments });
+    new CommentList({
+      $target: $page.querySelector(".post_comment_wrap"),
+      p_id: p_id,
+      init: this.state.comments,
+    });
   };
 
   selectPostDetail();
--- src/UploadPage.js
+++ src/UploadPage.js
@@ -1,72 +1,84 @@
-import { getUnsplash, getUploadPost } from './apis/api.js';
-import Header from './Header.js';
-import { routeChange } from './router.js';
+import { getUnsplash, getUploadPost } from "./apis/api.js";
+import Header from "./Header.js";
+import { routeChange } from "./router.js";
 
 export default function UploadPage({ $target }) {
-  const $page = document.createElement('section');
-  $page.id = 'upload_page';
+  const $page = document.createElement("section");
+  $page.id = "upload_page";
 
-  this.state = { image: '', title: '', content: '' };
+  this.state = { image: "", title: "", content: "" };
   this.setState = (new_state) => {
     this.state, (this.state[new_state.name] = new_state.value);
   };
 
-  $page.addEventListener('click', async (e) => {
-    if (e.target.id == 'img_upload_btn' && !e.target.classList.contains('done')) {
+  $page.addEventListener("click", async (e) => {
+    if (
+      e.target.id === "img_upload_btn" &&
+      !e.target.classList.contains("done")
+    ) {
       const data = await getUnsplash();
-      this.setState({ name: 'image', value: data.urls.regular });
-      let img_btn = document.querySelector('#img_upload_btn');
+      this.setState({ name: "image", value: data.urls.regular });
+      let img_btn = document.querySelector("#img_upload_btn");
       img_btn.innerHTML = ``;
       img_btn.style.backgroundImage = `url('${this.state.image}')`;
-      img_btn.style.backgroundSize = 'cover';
-      img_btn.style.backgroundPostion = 'center';
-      img_btn.classList.add('done');
+      img_btn.style.backgroundSize = "cover";
+      img_btn.style.backgroundPostion = "center";
+      img_btn.classList.add("done");
       await checkField();
     }
 
-    if (e.target.id === 'upload_btn' && e.target.classList.contains('active')) {
+    if (e.target.id === "upload_btn" && e.target.classList.contains("active")) {
       let option = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(this.state),
       };
       let result = await getUploadPost(option);
       if (result) {
         if (result.code === 201) {
-          routeChange('/');
+          routeChange("/");
         } else {
-          alert(result.msg);
-          return;
+          return alert(result.msg);
         }
       } else {
-        alert('게시물을 등록하지 못했습니다. \n 다시 시도해주세요.');
+        alert("게시물을 등록하지 못했습니다. \n다시 시도해주세요.");
         return;
       }
     }
   });
 
-  $page.addEventListener('keyup', async (e) => {
-    if (e.target.id === 'post_title') {
-      this.setState({ name: 'title', value: document.querySelector('#post_title').value });
-    } else if (e.target.id === 'post_content') {
-      this.setState({ name: 'content', value: document.querySelector('#post_content').value });
+  $page.addEventListener("keyup", async (e) => {
+    if (e.target.id === "post_title") {
+      this.setState({
+        name: "title",
+        value: document.querySelector("#post_title").value,
+      });
+    } else if (e.target.id === "post_content") {
+      this.setState({
+        name: "content",
+        value: document.querySelector("#post_content").value,
+      });
     }
     await checkField();
   });
 
   const checkField = async () => {
-    let btn = document.querySelector('#upload_btn');
-    if (this.state.img !== '' && this.state.title !== '' && this.state.content !== '') {
-      btn.classList.add('active');
+    let btn = document.querySelector("#upload_btn");
+    if (
+      this.state.img !== "" &&
+      this.state.title !== "" &&
+      this.state.content !== ""
+    ) {
+      btn.classList.add("active");
       return;
     } else {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
       return;
     }
   };
 
   this.render = () => {
-    new Header({ $target: $page, type: 'detail' });
+    new Header({ $target: $page, type: "detail" });
     $page.innerHTML += `<div class="upload_inner">
       <div id="img_upload_btn">
         <span></span>
