021427355145b42612e7a33bc52b3aa75334367c
jwkim <jwk0341@gmail.com>
2023-01-19 16:08:35 +0900

    [mod] 포트 변경

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
-app.use(express.static(path.join(__dirname, 'src')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
